d4f7d05031cbd42d25089d69b52c19e7192cc5b7
jaonjohnkim <kim.jaon79@gmail.com>
2017-08-23 15:54:03 -0700

    Functional Shared Stack Passed up to test 1

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+    
+    //generate object
+  var someInstance = {
+    storage: {},
+    count: 0
+  };
+  //assign properties
+  //borrow methods
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function () {
+    // this.
+  },
+  pop: function () {
+    // this.
+  },
+  size: function () {
+    return this.count;
+  }
+
+  
+};
 
 
