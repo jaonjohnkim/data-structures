a06f6b1312ecc296bc39a66890c45ff433dffed4
jaonjohnkim <kim.jaon79@gmail.com>
2017-08-23 14:51:42 -0700

    Functional Pattern Stack Passed up to test 3

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,18 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return Object.keys(storage).length;
+    return count;
   };
 
   return someInstance;
