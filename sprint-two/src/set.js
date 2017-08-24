30187ff69a52db7a473aa2b00d3b400b477ac086
jaonjohnkim <kim.jaon79@gmail.com>
2017-08-24 16:32:03 -0700

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] === undefined) { 
+    return false;
+  } else {
+    return true;
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage !== undefined) {
+    delete this._storage[item];
+  }  
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//Constant time for all three
