dc95a5da3e59e51c0dcbc2f1d45531e77cdc113b
jaonjohnkim <kim.jaon79@gmail.com>
2017-08-25 11:42:57 -0700

    Refactored hashTable using internal array

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,43 +7,75 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var currentValue = this._storage.get(index);
-  
-  if (currentValue !== undefined) { //if there is already something in that index
-    var key = Object.keys(currentValue)[0];
-    if (key !== k) { //if the keys are different 
-      // console.log('The key at the storage: ' + key + ', and k : ' + k);
-      var newType = {};
-      newType[key] = currentValue[key];
-      newType[k] = v;
-      this._storage.set(index, newType);
-    } else { //if keys are the same, then simply overwrite
-      var keyVal = {};
-      keyVal[k] = v;
-      this._storage.set(index, keyVal);
+  var firstLinkedList = this._storage.get(index);
+  // if the index is undefined
+  if (firstLinkedList === undefined) {
+    // if yes, add a linked list that points to an empty array
+    this._storage.set(index, {next: []});
+  } else {
+  // if there is something there, it is a linked list
+    for (var i = 0; i < firstLinkedList.next.length; i++) { // then iterate thru the array
+      // if key in the internal linked list matches our key
+      if (firstLinkedList.next[i].next[0] === k) {
+        // update the value
+        firstLinkedList.next[i].next[1] = v;
+        // return
+        return;
+      }
     }
-  } else { // if nothing at that storage spot
-    var keyVal = {};
-    keyVal[k] = v;
-    this._storage.set(index, keyVal);
   }
+  // create a linked list of our k, v and push into the array
+  var newLinkedList = {next: [k, v]};
+  this._storage.get(index).next.push(newLinkedList);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var getThis = this._storage.get(index);
-  if (getThis === undefined) {
+  //run get function to access the storage at that index
+  var firstLinkedList = this._storage.get(index);
+  //if it is a linked list
+  if (firstLinkedList !== undefined) {
+    //if the linked list is empty
+    if (firstLinkedList.next.length === 0) {
+      //return undefined
+      return undefined;
+    //if the linked list is not empty
+    } else {
+      //for loop
+      for (var i = 0; i < firstLinkedList.next.length; i++) {
+        //access the key at the second index of the linked list
+        var key = firstLinkedList.next[i].next[0];
+        //if the keys match
+        if (key === k) {
+          //return the value
+          return firstLinkedList.next[i].next[1];
+        }
+      //end
+      }
+      //once the loop checks for all the keys, and finds no match, then
+      return undefined;
+    }
+  //if it is not a linked list/if it is empty
+  } else {
     return undefined;
   }
-  return getThis[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function (value, i, storage) {
-    if (storage[i] !== undefined) {
-      if (Object.keys(storage[i])[0] === k) {
-        storage.splice(i, 1);
+  this._storage.each(function (linkedList, i, storage) {
+    //check if linked list is undefined
+    if (linkedList) {
+      //assume that insert had populated that linkedlist 
+      //with an array with at least 1 linkedlist 
+      //iterate through the array
+      for (var j = 0; j < linkedList.next.length; j++) {
+        //go into the linked list by .next, and get the 0 index for key
+        //if key matches
+        if (linkedList.next[j].next[0] === k) {
+          //delete the linkedlist
+          linkedList.next.splice(j, 1);
+        }  
       }
     }
   });
@@ -52,4 +84,5 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-// linear time complexity
+// look up is constant time operation
+// remove is linear time operation
